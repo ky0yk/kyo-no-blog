84946a73668ffde6497091f074541e5cacb6f70f
ky0yk <40429527+ky0yk@users.noreply.github.com>
2023-12-24 12:36:30 +0900

    サイトメタデータを修正

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,23 +1,23 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'Kyo No Blog',
+  author: 'Kyo',
+  headerTitle: 'Kyo No Blog',
+  description: '砂場',
+  language: 'ja',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://kyo-no-blog.vercel.app',
+  siteRepo: 'https://github.com/ky0yk/kyo-no-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
+  github: 'https://github.com/ky0yk',
+  twitter: 'https://twitter.com/recursion1024',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  locale: 'ja-JP',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
